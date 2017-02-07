cd222e9612ab17696358b29a245f2923186a0a3c
Marcelo <marceloremido@hotmail.com>
2017-02-07 17:29:28 -0200

    commit automatico

---
+++ src/test/java/br/com/testes/main/TesteDecisao.java
@@ -0,0 +1,25 @@
+package br.com.testes.main;
+
+public class TesteDecisao {
+	
+	
+	
+		  public static void main(String[] args) {
+			
+			  
+			  teste();	
+			  
+		  }
+	
+		  
+		  public static void teste(){
+			  
+			  if(7 > 15  ){
+				  System.out.println("É maior");
+			  }else{
+				  System.out.println("É menor");
+			  }
+			  
+		  }
+		  
+}
