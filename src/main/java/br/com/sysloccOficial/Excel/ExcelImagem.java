49bd36b90af82c1dec3b2f4d89e6e0cc3d40ee76
Marcelo <marceloremido@hotmail.com>
2017-03-23 11:49:03 -0300

    commit automatico

--- src/main/java/br/com/sysloccOficial/Excel/ExcelImagem.java
+++ src/main/java/br/com/sysloccOficial/Excel/ExcelImagem.java
@@ -1,22 +1,20 @@
 package br.com.sysloccOficial.Excel;
 
-import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 
-import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
 
 import org.apache.poi.ss.usermodel.ClientAnchor;
 import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.Drawing;
 import org.apache.poi.ss.usermodel.Picture;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.util.IOUtils;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.env.JOptCommandLinePropertySource;
 
 public class ExcelImagem {
 	
@@ -25,7 +23,7 @@ public class ExcelImagem {
 //		InputStream is = new FileInputStream("C:/SYSLOC/upload/logoEmpresas/logoExcelAgencia.png");
 		InputStream is = new FileInputStream(caminhoImagem);
 	    byte[] bytes = IOUtils.toByteArray(is);
-	    int pictureIdx = excel.addPicture(bytes, Workbook.PICTURE_TYPE_JPEG);
+	    int pictureIdx = excel.addPicture(bytes, excel.PICTURE_TYPE_JPEG);
 	    is.close();
 
 	    CreationHelper helper = excel.getCreationHelper();
@@ -37,15 +35,14 @@ public class ExcelImagem {
 	    ClientAnchor anchor = helper.createClientAnchor();
 	    //set top-left corner of the picture,
 	    //subsequent call of Picture#resize() will operate relative to it
-	    anchor.setAnchorType(3);
-/*	    anchor.setCol2(1);
-	    anchor.setRow1(1);
-	    anchor.setRow2(3);
-*/	    
+	    anchor.setCol1(0);
+	    anchor.setCol2(0);
+	    anchor.setRow1(3);
+	    
 	    Picture pict = drawing.createPicture(anchor, pictureIdx);
 
 	    //auto-size picture relative to its top-left corner
-	    pict.resize(0.5,0.5);
+	    pict.resize(tamanhoImagem);
 	    //JOptionPane.showMessageDialog(null, ""+tamanhoImagem);
 	   /* pict.resize(0.67);*/
 	}
--- src/main/java/br/com/sysloccOficial/ListaProducao/Excel/Galderma/GeraCorpoCenarios.java
+++ src/main/java/br/com/sysloccOficial/ListaProducao/Excel/Galderma/GeraCorpoCenarios.java
@@ -37,7 +37,7 @@ public class GeraCorpoCenarios {
 		cenario = excelGalderma.createSheet(nomeAba); /** Cria Aba Cenarios da planilha */
 		cenario.setZoom(80);
 		
-		ExcelImagem.InsereImagem(excelGalderma, cenario, "C:/SYSLOC/upload/logoEmpresas/logoExcelAgencia2.png",0.35);
+		ExcelImagem.InsereImagem(excelGalderma, cenario, "C:/SYSLOC/upload/logoEmpresas/logoExcelAgencia2.png",0.67);
 	
 		GeraCabecalhoExcelGalderma.geraCabecalhoFormula(cenario, excelGalderma, nomeAba, job);
 
