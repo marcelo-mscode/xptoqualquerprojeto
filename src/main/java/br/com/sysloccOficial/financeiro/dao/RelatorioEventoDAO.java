c7aa163403602a8949ed5eba8dd8ea6240d4acb7
marcelo <marceloremido@hotmail.com>
2018-03-06 10:52:04 -0300

    commit automatico

--- src/main/java/br/com/sysloccOficial/financeiro/dao/RelatorioEventoDAO.java
+++ src/main/java/br/com/sysloccOficial/financeiro/dao/RelatorioEventoDAO.java
@@ -268,13 +268,9 @@ public class RelatorioEventoDAO {
 	public void salvaCacheDoEvento(RelatorioEventos relatorioEvento){
 		
 		
-	/*	TypedQuery<CacheEvento> cacheTeste = manager.createQuery("SELECT c FROM CacheEvento c WHERE relatorioEvento="+relatorioEvento.getIdRelatorioEvento(), CacheEvento.class);
-		
+		/*	TypedQuery<CacheEvento> cacheTeste = manager.createQuery("SELECT c FROM CacheEvento c WHERE relatorioEvento="+relatorioEvento.getIdRelatorioEvento(), CacheEvento.class);
 		List<CacheEvento> caches = cacheTeste.getResultList();
-		
-		
 		*/
-		
 		
 		
 		manager.createQuery("DELETE FROM CacheEvento WHERE relatorioEvento="+relatorioEvento.getIdRelatorioEvento()).executeUpdate();
