6e7b1745b819bc4a5ae28e184bc7360fb6c3d22e
Marcelo Souza <marceloremido@hotmail.com>
2018-05-28 16:56:03 -0300

    commit do SSD automatico

--- gitProjeto.bat
+++ gitProjeto.bat
@@ -1,6 +1,6 @@
 cd C:\Users\windowsFast\Downloads\Syslocc - github\xptoqualquerprojeto
 git add .
-git commit -am "commit automatico"
+git commit -am "commit do SSD automatico"
 git push -u origin master
 
 
