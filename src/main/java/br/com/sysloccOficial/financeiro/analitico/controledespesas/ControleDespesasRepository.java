56d6e2ca2fb565c2af6cf1f5784e768325759832
Marcelo Souza <marceloremido@hotmail.com>
2018-08-22 09:13:03 -0300

    Commit automatico

--- src/main/java/br/com/sysloccOficial/financeiro/analitico/controledespesas/ControleDespesasRepository.java
+++ src/main/java/br/com/sysloccOficial/financeiro/analitico/controledespesas/ControleDespesasRepository.java
@@ -1,9 +1,15 @@
 package br.com.sysloccOficial.financeiro.analitico.controledespesas;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+
+import br.com.sysloccOficial.financeiro.model.ControleDespesas;
 
 
 @Repository
@@ -11,6 +17,21 @@ public class ControleDespesasRepository {
 
 	
 	@PersistenceContext	private EntityManager manager;
+	
+	
+	public List<ControleDespesas> listaControleDespesas() throws SQLException {
+		
+		try {
+			TypedQuery<ControleDespesas> query = manager.createQuery("FROM ControleDespesas",ControleDespesas.class);
+			return query.getResultList();
+		} catch (Exception e) {
+			new 
+		}
+		
+		
+		
+	}
+	
 	
 	
 	
