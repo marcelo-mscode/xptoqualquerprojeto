458b6ea8e445730f3640174090d74d3868f2defb
Marcelo Souza <marceloremido@hotmail.com>
2018-08-22 19:01:04 -0300

    Commit automatico

--- src/main/java/br/com/sysloccOficial/financeiro/analitico/controledespesas/ControleDespesasController.java
+++ src/main/java/br/com/sysloccOficial/financeiro/analitico/controledespesas/ControleDespesasController.java
@@ -1,16 +1,27 @@
 package br.com.sysloccOficial.financeiro.analitico.controledespesas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 import br.com.sysloccOficial.financeiro.model.ControleDespesas;
 
 @Controller
+@Transactional
 public class ControleDespesasController {
 	
 	@Autowired ControleDespesasService controleServices;
+	@PersistenceContext	private EntityManager manager;
 	
 	
 	@RequestMapping("controleDespesas")
@@ -28,18 +39,26 @@ public class ControleDespesasController {
 	}
 	
 	@RequestMapping("salvaControleDespesas")
-	public String salvaControleDespesas(ControleDespesas controleNovo){
+	public String salvaControleDespesas(ControleDespesas controleNovo) throws ParseException{
 		
 		int idAnalitico = 123;
 		
 		System.out.println(controleNovo.getDataTransiente());
 		
-		try {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date data = new java.sql.Date(format.parse(controleNovo.getDataTransiente() + " 00:00:00").getTime());
+		controleNovo.setData(data);
+		
+		manager.merge(controleNovo);
+		
+		
+		
+/*		try {
 			controleServices.salvaNovoControleDespesas(controleNovo);
 		} catch (Exception e) {
 			System.out.println("");
 		}
-		
+*/		
 		
 		return "redirect:controleDespesas?idAnalitico="+idAnalitico;
 		
